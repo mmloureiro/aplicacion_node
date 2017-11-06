aff059e86fdf80e4d2d378f30dc3d16758bfecf1
Marcos Martínez <mmloureiro@hotmail.es>
2017-11-06 16:57:13 +0100

    Añadidas funciones para la importación y el volcado de la BD

--- routes/index.js
+++ routes/index.js
@@ -10,30 +10,36 @@ router.get('/', function(req, res, next) {
 
 // POST
 router.post('/users', function(req,res,next){
-	var host = req.body.host
-	var user = req.body.usuario
-	var pass = req.body.passw
-	var bd = req.body.bd
-	var port = req.body.puerto
-// creamos el nombre del archivo que vamos a usar para guardar los datos de la BD
-	var archivo_sql = '../sql/' + host + '_' + bd + '_' + volcado.nombre_archivo()
-	console.log(archivo_sql)
-	res.render('users', {host: host, user: user, pass: pass, bd: bd, port: port, archivo_sql: archivo_sql})
+	var host1 = req.body.host1
+	var user1 = req.body.usuario1
+	var pass1 = req.body.passw1
+	var bd1 = req.body.bd1
+	var port1 = req.body.puerto1
 
-	volcado.importar(host, user, pass, bd, port, archivo_sql)
+	var host2 = req.body.host2
+	var user2 = req.body.usuario2
+	var pass2 = req.body.passw2
+	var bd2 = req.body.bd2
+	var port2 = req.body.puerto2
+// creamos el nombre del archivo que vamos a usar para guardar los datos de la BD
+	var archivo_sql = './public/sql/' + host1 + '_' + bd1 + '_' + volcado.nombre_archivo()
+	console.log(archivo_sql)
+	res.render('users', {host: host1, user: user1, pass: pass1, bd: bd1, port: port1, archivo_sql: archivo_sql})
+
+	volcado.importar(host1, user1, pass1, bd1, port1, archivo_sql)
 		.then(function(){
 		volcado.mod_archivo(archivo_sql)})
-		// .then(function(){
-			// volcado.crearBD(host2, user2, pass2, port2, bd2, archivo_sql)})
-		// .then(function(){		
-			// volcado.enviar(host2, user2, pass2, port2, bd2, archivo_sql)})
+		.then(function(){
+			volcado.crearBD(host2, user2, pass2, port2, bd2, archivo_sql)})
+		.then(function(){		
+			volcado.enviar(host2, user2, pass2, port2, bd2, archivo_sql)})
 		// .then(function(){
 			// volcado.borrar_archivo(archivo_sql)})
 		.then(function(response){
 			console.log("TODO ES CORRECTO",response)
 		})
 		.catch(function(err){
-			console.log(err,"No se pudo completar la importación")
+			console.log(err,"No se pudo completar la acción")
 		})
 })
 
